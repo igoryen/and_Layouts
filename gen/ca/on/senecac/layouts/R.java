0f51bfc9659576db46a344238dddbace2dd30788
igoryen <igor.yentaltsev@gmail.com>
2014-02-05 20:16:05 -0500

    Layouts: added an image to display

--- gen/ca/on/senecac/layouts/R.java
+++ gen/ca/on/senecac/layouts/R.java
@@ -23,30 +23,33 @@ public final class R {
     public static final class drawable {
         public static final int android=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int yggy=0x7f020002;
     }
     public static final class id {
-        public static final int TextView04=0x7f080013;
-        public static final int action_settings=0x7f080015;
-        public static final int backbutton=0x7f080002;
-        public static final int button=0x7f080001;
-        public static final int button1=0x7f080007;
-        public static final int button2=0x7f080014;
-        public static final int button3=0x7f080000;
-        public static final int button4=0x7f08000d;
-        public static final int button5=0x7f08000e;
-        public static final int button6=0x7f080010;
-        public static final int button7=0x7f080011;
-        public static final int checkBox1=0x7f08000f;
-        public static final int editFirstName=0x7f080004;
-        public static final int editLastName=0x7f080005;
-        public static final int firstName=0x7f080003;
-        public static final int lastName=0x7f080006;
-        public static final int progressBar1=0x7f080008;
-        public static final int radioButton1=0x7f08000a;
-        public static final int radioButton2=0x7f080009;
-        public static final int seekBar1=0x7f08000c;
-        public static final int tableRow1=0x7f080012;
-        public static final int toggleButton1=0x7f08000b;
+        public static final int TextView04=0x7f080015;
+        public static final int action_settings=0x7f080017;
+        public static final int backbutton=0x7f080004;
+        public static final int button=0x7f080003;
+        public static final int button1=0x7f080009;
+        public static final int button2=0x7f080016;
+        public static final int button3=0x7f080002;
+        public static final int button4=0x7f08000f;
+        public static final int button5=0x7f080010;
+        public static final int button6=0x7f080012;
+        public static final int button7=0x7f080013;
+        public static final int checkBox1=0x7f080011;
+        public static final int editFirstName=0x7f080006;
+        public static final int editLastName=0x7f080007;
+        public static final int firstName=0x7f080005;
+        public static final int gallery1=0x7f080000;
+        public static final int lastName=0x7f080008;
+        public static final int progressBar1=0x7f08000a;
+        public static final int radioButton1=0x7f08000c;
+        public static final int radioButton2=0x7f08000b;
+        public static final int seekBar1=0x7f08000e;
+        public static final int tableRow1=0x7f080014;
+        public static final int toggleButton1=0x7f08000d;
+        public static final int yggy=0x7f080001;
     }
     public static final class layout {
         public static final int frame_main=0x7f030000;
@@ -62,6 +65,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int yggy_photo=0x7f050003;
     }
     public static final class style {
         /** 
@@ -91,4 +95,25 @@ public final class R {
          */
         public static final int AppTheme=0x7f060001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a Gallery1.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #Gallery1_android_galleryItemBackground android:galleryItemBackground}</code></td><td></td></tr>
+           </table>
+           @see #Gallery1_android_galleryItemBackground
+         */
+        public static final int[] Gallery1 = {
+            0x0101004c
+        };
+        /**
+          <p>This symbol is the offset where the {@link android.R.attr#galleryItemBackground}
+          attribute's value can be found in the {@link #Gallery1} array.
+          @attr name android:galleryItemBackground
+        */
+        public static final int Gallery1_android_galleryItemBackground = 0;
+    };
 }
